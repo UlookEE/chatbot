10d91f05179feac47c261d36ad02aa034c4dfbc1
Lee Hae Kyun <dlgprbs9441@gmail.com>
2020-01-19 15:09:39 +0900

    Example 1 update

---
+++ example 1.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "http://example.com/"
+
+response = requests.get(url)
+
+
+data = response.text
+
+soup = BeautifulSoup(data, 'html.parser')
+
+tags = soup.find_all('a')
+
+for tag in tags:
+    print(tag.get('href'))
+
+for tag in tags:
+    print(tag)
+
